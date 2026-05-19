9ac8d44ae31011423675613428aa0b7939c8f71f
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-05-19 18:39:49 +0700

    menambahkan class baseClass menggunakan final dan virtual

---
+++ noPewarisan.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;   
+
+class baseClass final {  //final digunakan untuk mencegah pewarisan
+    public:
+    virtual void perkenalan() {
+        cout << "Halo saya function dari base class";
+    }
+};
